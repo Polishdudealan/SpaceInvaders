abe12aa9aaf67f2ddbba8165676c63365210cf12
Robert Burke <rwburke@me.com>
2019-11-22 23:53:45 -0500

    Added all level data (untested)

--- distribution_code/distribution_code.ino
+++ distribution_code/distribution_code.ino
@@ -56,6 +56,12 @@ const Color WHITE(4, 4, 4);
 const Color LIME(2, 4, 0);
 const Color AQUA(0, 4, 4);
 
+static const int LEVEL_DATA[4][2][8] = 
+	{{{1, 1, 1, 1, 1, 1, 1, 1}, {0, 0, 0, 0, 0, 0, 0, 0}},
+	{{1, 2, 1, 2, 1, 2, 1, 2}, {2, 1, 2, 1, 2, 1, 2, 1}},
+	{{1, 2, 3, 4, 5, 1, 2, 3}, {4, 5, 1, 2, 3, 4, 5, 1}},
+	{{5, 4, 5, 4, 5, 4, 5, 4}, {2, 3, 2, 3, 2, 3, 2, 3}}};
+
 class Invader {
   public:
     // Constructors
@@ -346,6 +352,11 @@ class Game {
     void reset_level() {
       matrix.fillScreen(matrix.Color333(0, 0, 0));
       level++;
+      for (int i = 0; i < 2; i++){
+	for (int j = 0; j < 8; j++){
+	  enemies[i*8+j] = Invader(j, i, LEVEL_DATA[level][i][j]);
+	}
+      }
       print_level(level);
     }
 };
